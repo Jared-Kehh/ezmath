611e11c8bb3314484f4e923ac8c3aadbf5471152
Jared Keh <keh19001@byui.edu>
2024-10-29 13:03:58 -0600

    a few updates

--- ezmath/src/App.js
+++ ezmath/src/App.js
@@ -23,24 +23,73 @@ function AppHeader() {
   );
 }
 
+function buttonFunc(){
+  document.getElementById("myDropdown").classList.toggle("show")
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 function AppBody() {
   return(
     <div className='MathSubjects'>
+      <div id='about'>
+        <h1>What is this about?</h1>
+      </div>
+      <div id='aboutsec'>
+        <h3>These concepts can be a little hard for students to learn so 
+          this will make each math concept easy to understand. Each section
+          will have a game and a short quiz that can help them to refresh
+          their math skills and become proficient at them.</h3>
+      </div>
       <div id='subject'>
         <h1>Addition</h1>
         <h3>This is a Math Adding Game!</h3>
+        <button onclick="buttonFunc()" class="dropbtn"><a href='#'>Play!</a></button>
+        <div id="myDropdown" class="dropdown-content">
+          <a href='#'>Easy</a>
+          <a href='#'>Medium</a>
+          <a href='#'>Hard</a>
+        </div>
       </div>
       <div id='subject'>
         <h1>Subtraction</h1>
         <h3>This is a Math Subtracting Game!</h3>
+        <button onclick="buttonFunc()" class="dropbtn"><a href='#'>Play!</a></button>
+        <div id="myDropdown" class="dropdown-content">
+          <a href='#'>Easy</a>
+          <a href='#'>Medium</a>
+          <a href='#'>Hard</a>
+        </div>
       </div>
       <div id='subject'>
         <h1>Multiplication</h1>
         <h3>This is a Math Multiplying Game!</h3>
+        <button onclick="buttonFunc()" class="dropbtn"><a href='#'>Play!</a></button>
+        <div id="myDropdown" class="dropdown-content">
+          <a href='#'>Easy</a>
+          <a href='#'>Medium</a>
+          <a href='#'>Hard</a>
+        </div>
       </div>
       <div id='subject'>
         <h1>Division</h1>
         <h3>This is a Math Dividing Game!</h3>
+        <button onclick="buttonFunc()" class="dropbtn"><a href='#'>Play!</a></button>
+        <div id="myDropdown" class="dropdown-content">
+          <a href='#'>Easy</a>
+          <a href='#'>Medium</a>
+          <a href='#'>Hard</a>
+        </div>
       </div>
     </div>
   );
@@ -51,7 +100,6 @@ function AppFooter() {
     <footer>
       <div className='about'>
         <h4>&copy; ezMath</h4>
-
       </div>
     </footer>
   );
